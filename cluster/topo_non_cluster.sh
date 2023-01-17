182a8b13e626b015a9dafeb4ce264296569e3af5
w41ter <w41ter.l@gmail.com>
2023-01-17 18:24:06 +0800

    fix topy non cluster setup

--- cluster/topo_non_cluster.sh
+++ cluster/topo_non_cluster.sh
@@ -4,7 +4,7 @@ source config.sh
 source cluster/config.sh
 
 echo "Master node is ${INIT_NODE}"
-for HOST in ${HOST_KVROCKS_NODES}; do
+for host in ${HOST_KVROCKS_NODES[@]}; do
     if [[ $host == ${INIT_NODE} ]]; then
         redis-cli -p ${KVROCKS_PORT} -h ${host} slaveof no one
     else
