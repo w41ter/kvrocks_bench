b0cd2ea16f2f293475bbb9e7cc608ccc383c39a3
w41ter <w41ter.l@gmail.com>
2023-01-18 11:34:43 +0800

    Allow aws root login

--- setup_connection.sh
+++ setup_connection.sh
@@ -3,6 +3,16 @@
 # This script will establish ssh authorization for the central node.
 source config.sh
 
+allow_login_commands="sudo su <<EOF
+sed -i 's/no-port-forwarding,no-agent-forwarding,no-X11-forwarding,command=\"echo.*exit 142\"//g' ~/.ssh/authorized_keys;
+EOF"
+
+if [ ! -z $AWS_USER ]; then
+    echo "Need enable ${USER} login for AWS server"
+
+    ssh ${AWS_USER}@${HOST_CENTRAL} "${allow_login_commands}"
+fi
+
 # Install rsa keys
 ssh-keygen -f ~/.ssh/known_hosts -R ${HOST_CENTRAL} >/dev/null
 ssh-keyscan ${HOST_CENTRAL} >> ~/.ssh/known_hosts; >/dev/null
@@ -16,9 +26,21 @@ Host *
 EOF"
 
 for host in ${HOSTS[@]}; do
+    if [[ $host == ${HOST_CENTRAL_PRIVATE} ]]; then
+        continue
+    fi
+
     ssh ${USER}@${HOST_CENTRAL} "\
+        touch -f ~/.ssh/known_hosts; \
+        chmod 0644 ~/.ssh/known_hosts; \
         ssh-keygen -f '~/.ssh/known_hosts' -R ${host}; \
         ssh-keyscan ${host} >> ~/.ssh/known_hosts; \
-    "
+    " </dev/null
+
+    # allow root login
+    if [ ! -z ${AWS_USER} ]; then
+        ssh -J ${USER}@${HOST_CENTRAL} ${AWS_USER}@${host} "${allow_login_commands}" </dev/null
+    fi
+
 	ssh-copy-id -i id_rsa.pub -o StrictHostKeyChecking=no -o ProxyJump=${USER}@${HOST_CENTRAL} ${USER}@${host} </dev/null
 done
