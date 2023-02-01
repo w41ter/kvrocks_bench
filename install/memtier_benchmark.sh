38ab37b7d1b41dcf90b716cb41f5ec142b3206fb
w41ter <w41ter.l@gmail.com>
2023-02-01 10:08:00 +0800

    add zipf patch

--- install/memtier_benchmark.sh
+++ install/memtier_benchmark.sh
@@ -3,6 +3,8 @@
 apt-get install -y libpcre3-dev libevent-dev pkg-config zlib1g-dev libssl-dev
 git clone https://github.com/RedisLabs/memtier_benchmark.git
 cd memtier_benchmark
+wget https://github.com/RedisLabs/memtier_benchmark/pull/166.patch
+git am 166.patch
 autoreconf -ivf
 ./configure
 make -j`nproc`
